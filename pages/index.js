a5811be681449f9e5e196d78b26fd4c698f01aaf
Philipp Gampe <phil@MacBook-Air.local>
2022-11-16 11:43:49 +0100

    rename Image

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
 
         {/* Background Image */}
 
-        <Image src="/Wetter-Bg.jpg" layout="fill" className="object-cover" />
+        <Image src="/wetter-bg.jpg" layout="fill" className="object-cover" />
 
         {/* Search */}
 
